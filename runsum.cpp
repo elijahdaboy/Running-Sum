36afedb47ee67b1d442ddcae20bf56b89f1a88ab
Elijah Aaron <elijahhager20@gmail.com>
2025-04-21 09:48:36 -0600

    Add files via upload

---
+++ runsum.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+// find running sum of vector
+
+std::vector<int> nums = {1, 2, 3, 4, 5};
+
+int main(){
+    for (int i = 0; i <= nums.size(); i++){
+        if (i > 0){
+            nums[i] = nums[i] + nums[i - 1]; // <-- ts pmo
+        } else {
+            nums[0] = nums[0];
+        }
+    }
+    for (int i = 0; i < nums.size(); i++)
+    std::cout << nums[i] << '\n';
+}
